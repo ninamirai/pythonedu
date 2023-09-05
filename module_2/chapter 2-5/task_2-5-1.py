2f05571a7a84bee1fb9cfea2ca19495a9e5e6352
nina <johndoe@example.com>
2023-09-06 00:56:26 +0300

    chapter 2-5

---
+++ module_2/chapter 2-5/task_2-5-1.py
@@ -0,0 +1,8 @@
+# Геометрическая прогрессия
+
+b_1 = int(input())
+q = int(input())
+n = int(input())
+
+b_n = b_1*q**(n-1)
+print(b_n)
---
+++ module_2/chapter 2-5/task_2-5-3.py
@@ -0,0 +1,10 @@
+#  Мандарины
+
+n = int(input())
+k = int(input())
+
+received_man = k//n
+balance = k%n
+print(received_man)
+print(balance)
+    
